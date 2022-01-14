6857a656f90bf62aa3234b5a0ab93d99d575a50d
melyssy <ksenyatokareva@gmail.com>
2022-01-14 22:39:12 +0300

    fix merging logic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import dotenv from "dotenv";
+import _ from 'lodash';
 import FormData from "form-data";
 import { createReadStream, readFileSync } from "fs";
 
@@ -8,19 +9,15 @@ dotenv.config();
 // add agent name, id and group to get tickets from. the name of the group should match .json file
 const agents = [
   {
-    name: 'Jane Doe',
-    id: 2222,
-    group: 'group 1'
-  },
-  {
     name: 'John Doe',
-    id: 1111,
-    group: 'group 2'
-  }
+    id: 9876,
+    group: 'group_1'
+  },
 ];
-const minutes = 1;
+const minutes = 15; 
+const ticketsPerAgent = 16; // 16 for 4 hours
 
-const groups = agents.map((agent) => agent.group);
+const groups = [...new Set(agents.map((agent) => agent.group))];
 const createTicketUrl = process.env.URL;
 let messages;
 let currMsgId = 0;
@@ -43,7 +40,7 @@ const makeFormData = ({ subject, messagePath, agent, files }) => {
 const sendTicket = (url, formData) => {
   axios.post(url, formData, { headers: formData.getHeaders() })
   .then(({ data }) => {
-    console.log(`status: ${data.status}`);
+    console.log(data);
   });
 };
 
@@ -61,18 +58,22 @@ const run = () => {
 
 const prepData = (groups) => {
   const catalogues = groups.map((group) => {
-    const groupArr = JSON.parse(readFileSync(`./mocks/${group}.json`));
     const agentsInGroup = agents.filter(a => (a.group === group));
+    const groupArr = JSON.parse(readFileSync(`./mocks/${group}.json`));
     groupArr.forEach((obj, i) => {
       const idx = i >= agentsInGroup.length ? i % agentsInGroup.length : i;
       obj.agent = agentsInGroup[idx].id;
     });
-    return groupArr;
+    return { group, numOfAgents: agentsInGroup.length, tickets: groupArr };
   });
   const data = [];
-  for (let i = 0; i < catalogues[0].length; i += 1) {
-    for (let j = 0; j < catalogues.length; j += 1) {
-      data.push(catalogues[j][i]);
+  const pointers = groups.reduce((acc, group) => ({...acc, [group]: 0}), {});
+  for (let i = 0; i < ticketsPerAgent; i++) {
+    for (let j = 0; j < catalogues.length; j++) {
+      const { group, tickets, numOfAgents } = catalogues[j];
+      const id = pointers[group];
+      data.push(...tickets.slice(id, id + numOfAgents));
+      pointers[group] += numOfAgents;
     }
   }
   return data;
